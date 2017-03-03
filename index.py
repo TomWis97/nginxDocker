44288a1101522f7dc2b043d74d08ab0169ee8fa5
TomWis97 <github@tomwishaupt.nl>
2017-03-03 14:07:23 +0100

    Enhanced index.py

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+import cgitb
+import cgi
+import os
+import sys
+cgitb.enable()
 print("Content-type: text/html\n\n")
 print("<h1>Hello World!</h1>")
+print("<h2>Python version: {}</h2>".format(sys.version))
+print("<h2>REQUEST_URI: {}</h2>".format(os.environ['REQUEST_URI']))
+print("<code>")
+for i in dir(cgi):
+    print(i)
+print("<code>" + str(dir(cgi)) + "</code>")
+cgi.test()
